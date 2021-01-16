4a19b4faedc0c5262f1d0a0c8d45e33e78f48626
NEOEXEUNTAR\zrats <zratsewk@yahoo.com>
2021-01-16 12:28:41 -0700

    Initial Code Commit

---
+++ Models/DatabaseSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFManager.Models
+{
+  public class DatabaseSettings : IDatabaseSettings
+  {
+    public string DatabaseName { get; set; }
+    public string ConnectionString { get; set; }
+
+    public string CollectionName_Campaigns { get; set; }
+  }
+
+  public interface IDatabaseSettings
+  {
+    string DatabaseName { get; set; }
+    string ConnectionString { get; set; }
+
+    string CollectionName_Campaigns { get; set; }
+  }
+}
---
+++ Services/CampaignService.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+using PFManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PFManager.Services
+{
+  public class CampaignService
+  {
+    private readonly IMongoCollection<Campaign> _campaigns;
+
+    public CampaignService(IDatabaseSettings settings)
+    {
+      var client = new MongoClient(settings.ConnectionString);
+      var database = client.GetDatabase(settings.DatabaseName);
+
+      _campaigns = database.GetCollection<Campaign>(settings.CollectionName_Campaigns);
+    }
+
+    public List<Campaign> Get() => _campaigns.Find(c => true).ToList();
+
+    public Campaign Get(string id) => _campaigns.Find(c => c.Id == id).FirstOrDefault();
+
+    public Campaign Create(Campaign campaign)
+    {
+      _campaigns.InsertOne(campaign);
+      return campaign;
+    }
+
+    public void Update(string id, Campaign campaign) => _campaigns.ReplaceOne(c => c.Id == id, campaign);
+
+    public void Remove(Campaign campaign) => _campaigns.DeleteOne(c => c.Id == campaign.Id);
+
+    public void Remove(string id) => _campaigns.DeleteOne(c => c.Id == id);
+  }
+}
